437f455c99c31db476b2cf2c5b6ded34e7b5790d
Sebastian Maniak <sebastian@maniak.io>
2023-11-15 01:08:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.name}-${random_string.random.id}-terraform-state"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
